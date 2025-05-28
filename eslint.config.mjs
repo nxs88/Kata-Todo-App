7f8b1e660a9b7d9a5a820e664bf2ee29d0fb42ff
nXs <nxs88@mail.ru>
2025-05-28 16:37:36 +0700

    update

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11Y from 'eslint-plugin-jsx-a11y';
+import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores(['**/node_modules/', '**/dist/', '**/build/']),
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        'airbnb',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier'
+      )
+    ),
+
+    plugins: {
+      react: fixupPluginRules(react),
+      'react-hooks': fixupPluginRules(reactHooks),
+      'jsx-a11y': fixupPluginRules(jsxA11Y),
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: babelParser,
+    },
+
+    rules: {
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.js', '.jsx'],
+        },
+      ],
+
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+    ignores: ['./node_modules', './dist', './build'],
+  },
+]);
